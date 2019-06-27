e1cf218e00c80f27051ca2ccc289c2eba9dbf9e8
Pablo Abril <pablo.abril@cybelangel.com>
2019-06-27 12:08:48 +0200

    Service worker refresh on new content event

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 };
