627be6cef2acedb01ba4baef894f5a30025a8b2c
Pablo Abril <pabloabril75@gmail.com>
2020-03-30 17:15:13 +0200

    deploy

---
+++ precache-manifest.0021badd7cbc30bd8a89467116ba8e14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700e1dc69e71300cc2e5",
+    "url": "/css/app.00931879.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "229e3eb032ad6aec5fdab71242d0363a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c86a5b13e208f321ab3b",
+    "url": "/js/about.af07bc7c.js"
+  },
+  {
+    "revision": "700e1dc69e71300cc2e5",
+    "url": "/js/app.f35e6529.js"
+  },
+  {
+    "revision": "c1f6c1ab1b8fca65075a",
+    "url": "/js/chunk-vendors.caf862be.js"
+  },
+  {
+    "revision": "29ad291a3f5e0986e9f77ff4a6b53bfb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
